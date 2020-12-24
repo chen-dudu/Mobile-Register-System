82b6d791b576d683d66a20d8260e5ec5cf6d93ec
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-24 11:34:31 +0800

    added source files

---
+++ app/src/main/java/com/example/myapp/Repository/Manager.java
@@ -0,0 +1,4 @@
+package com.example.myapp.Repository;
+
+class Manager {
+}
---
+++ app/src/main/java/com/example/myapp/UI/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClickEnter(View view) {
+
+    }
+
+    public void onClickCheck(View view) {
+
+    }
+}
