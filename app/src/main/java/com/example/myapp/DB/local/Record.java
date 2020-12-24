35b7071ec9d201a4cfaffa4b41ecb6c6c061b273
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-24 13:34:24 +0800

    added constructor, and a new attribute 'tag'

--- app/src/main/java/com/example/myapp/DB/local/Record.java
+++ app/src/main/java/com/example/myapp/DB/local/Record.java
@@ -1,26 +1,52 @@
+/**
+ * this file contains the entity class, "Record", for the room database
+ */
 package com.example.myapp.DB.local;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class Record {
-    @PrimaryKey
-    public int ID;
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "ID")
+    public int id;
 
     @ColumnInfo(name = "Province")
-    public String province;
+    @NonNull
+    public String province = "";
 
     @ColumnInfo(name = "City")
-    public String city;
+    @NonNull
+    public String city = "";
 
     @ColumnInfo(name = "District")
-    public String district;
+    @NonNull
+    public String district = "";
 
     @ColumnInfo(name = "Road")
-    public String road;
+    @NonNull
+    public String road = "";
 
     @ColumnInfo(name = "Other detail")
-    public String detail;
+    @NonNull
+    public String detail = "";
+
+//    @ForeignKey(Tag.class)
+    @ColumnInfo(name = "Tag")
+    @NonNull
+    public String tag = "";
+
+    public Record(@NonNull String province, @NonNull String city, @NonNull String district,
+                  @NonNull String road, @NonNull String detail, @NonNull String tag) {
+        this.province = province;
+        this.city = city;
+        this.district = district;
+        this.road = road;
+        this.detail = detail;
+        this.tag = tag;
+    }
 }
