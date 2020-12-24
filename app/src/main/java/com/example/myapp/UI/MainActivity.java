88902902312d365f6820e85ddecac03f6f4d0c8d
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-24 17:03:24 +0800

    added click listener for buttons at the main page

--- app/src/main/java/com/example/myapp/UI/MainActivity.java
+++ app/src/main/java/com/example/myapp/UI/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,10 +19,26 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onClickEnter(View view) {
-
+        Context c = view.getContext();
+        Intent i = new Intent(c, EnterActivity.class);
+        c.startActivity(i);
     }
 
     public void onClickCheck(View view) {
+        Context c = view.getContext();
+        Intent i = new Intent(c, CheckActivity.class);
+        c.startActivity(i);
+    }
 
+    public void onClickCreateTag(View view) {
+        Context c = view.getContext();
+        Intent i = new Intent(c, CreateTagActivity.class);
+        c.startActivity(i);
+    }
+
+    public void onClickTags(View view) {
+        Context c = view.getContext();
+        Intent i = new Intent(c, TagsActivity.class);
+        c.startActivity(i);
     }
 }
