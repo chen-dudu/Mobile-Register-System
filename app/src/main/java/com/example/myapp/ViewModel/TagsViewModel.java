411d61ffb34bd912613a23ebbb30d26d79d3122d
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-25 14:41:12 +0800

    finished implementing tag view model

--- app/src/main/java/com/example/myapp/ViewModel/TagsViewModel.java
+++ app/src/main/java/com/example/myapp/ViewModel/TagsViewModel.java
@@ -1,4 +1,25 @@
 package com.example.myapp.ViewModel;
 
-class TagsViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.myapp.DB.local.Tag;
+import com.example.myapp.Repository.Manager;
+
+import java.util.List;
+
+public class TagsViewModel extends ViewModel {
+
+    private Manager manager;
+    private LiveData<List<Tag>> tags;
+
+    public TagsViewModel(Application application) {
+        this.manager = new Manager(application);
+    }
+
+    public LiveData<List<Tag>> getAllTags() {
+        return manager.getAllTags();
+    }
 }
