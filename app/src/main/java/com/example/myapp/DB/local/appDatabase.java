2108d0338dc76782157fbe13a0a0372fd04e34d9
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-25 14:42:03 +0800

    added mock data used to populate database

--- app/src/main/java/com/example/myapp/DB/local/appDatabase.java
+++ app/src/main/java/com/example/myapp/DB/local/appDatabase.java
@@ -2,9 +2,11 @@ package com.example.myapp.DB.local;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,11 +26,26 @@ public abstract class appDatabase extends RoomDatabase {
                 if (instance == null) {
                     instance = Room.databaseBuilder(c.getApplicationContext(),
                             appDatabase.class, "app_database")
+                            .addCallback(initialCallback)
                             .build();
                 }
             }
         }
         return instance;
-
     }
+
+    private static RoomDatabase.Callback initialCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            dbExecutor.execute(() -> {
+                TagDao tagDao = instance.tagDao();
+                tagDao.deleteAll();
+
+                tagDao.addTag(new Tag("标签A", "描述A"));
+                tagDao.addTag(new Tag("标签B", "描述B"));
+            });
+        }
+    };
 }
