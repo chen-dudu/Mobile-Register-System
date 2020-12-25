879e7590c244129d534c1a4c2f858165440c8eba
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-25 16:48:33 +0800

    added factory for TagDetailViewModel

---
+++ app/src/main/java/com/example/myapp/Util/TagDetailViewModelFactory.java
@@ -0,0 +1,24 @@
+package com.example.myapp.Util;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapp.ViewModel.TagDetailViewModel;
+
+public class TagDetailViewModelFactory implements ViewModelProvider.Factory {
+
+    private Application application;
+
+    public TagDetailViewModelFactory(Application application) {
+        this.application = application;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new TagDetailViewModel(this.application);
+    }
+}
