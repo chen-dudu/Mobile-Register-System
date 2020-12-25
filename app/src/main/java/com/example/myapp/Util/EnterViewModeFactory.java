71f1457b80b70151519b3dfddc2497b5c39620e0
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-25 16:51:44 +0800

    added factory for EnterViewModel

---
+++ app/src/main/java/com/example/myapp/Util/EnterViewModeFactory.java
@@ -0,0 +1,24 @@
+package com.example.myapp.Util;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapp.ViewModel.EnterViewModel;
+
+public class EnterViewModeFactory implements ViewModelProvider.Factory {
+
+    private Application application;
+
+    public EnterViewModeFactory(Application application) {
+        this.application = application;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new EnterViewModel(this.application);
+    }
+}
