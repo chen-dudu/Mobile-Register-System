55cdd7bf94ad5a4ecdc2004be19011ce06d7a585
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-25 16:45:05 +0800

    added view model for CreateTagActivity

--- app/src/main/java/com/example/myapp/ViewModel/CreateTagViewModel.java
+++ app/src/main/java/com/example/myapp/ViewModel/CreateTagViewModel.java
@@ -1,4 +1,21 @@
 package com.example.myapp.ViewModel;
 
-class CreateTagViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.ViewModel;
+
+import com.example.myapp.DB.local.Tag;
+import com.example.myapp.Repository.Manager;
+
+public class CreateTagViewModel extends ViewModel {
+
+    private Manager manager;
+
+    public CreateTagViewModel(Application application) {
+        manager = new Manager(application);
+    }
+
+    public void addTag(Tag tag) {
+        manager.addTag(tag);
+    }
 }
