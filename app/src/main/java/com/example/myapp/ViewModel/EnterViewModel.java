88a35bd42435051b98a975857f5dc8263f12f64f
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-28 16:14:39 +0800

    added tag retrieval to EnterViewModel

--- app/src/main/java/com/example/myapp/ViewModel/EnterViewModel.java
+++ app/src/main/java/com/example/myapp/ViewModel/EnterViewModel.java
@@ -2,10 +2,14 @@ package com.example.myapp.ViewModel;
 
 import android.app.Application;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.myapp.DB.local.Record;
+import com.example.myapp.DB.local.Tag;
 import com.example.myapp.Repository.Manager;
+
+import java.util.List;
 
 public class EnterViewModel extends ViewModel {
 
@@ -17,5 +21,9 @@ public class EnterViewModel extends ViewModel {
 
     public void addRecord(Record record) {
         manager.addRecord(record);
+    }
+
+    public LiveData<List<Tag>> getAllTags() {
+        return manager.getAllTags();
     }
 }
