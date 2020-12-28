b414a39fe85fdd1ab6b1c69fa95d3736dc83cc57
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-28 14:04:55 +0800

    finished implemented view model for EnterActivity

--- app/src/main/java/com/example/myapp/ViewModel/EnterViewModel.java
+++ app/src/main/java/com/example/myapp/ViewModel/EnterViewModel.java
@@ -1,4 +1,21 @@
 package com.example.myapp.ViewModel;
 
-class EnterViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.ViewModel;
+
+import com.example.myapp.DB.local.Record;
+import com.example.myapp.Repository.Manager;
+
+public class EnterViewModel extends ViewModel {
+
+    private Manager manager;
+
+    public EnterViewModel(Application application) {
+        this.manager = new Manager(application);
+    }
+
+    public void addRecord(Record record) {
+        manager.addRecord(record);
+    }
 }
