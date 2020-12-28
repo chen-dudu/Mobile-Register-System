63ddfdbb10b4c1a039cf7d411d97643bbae52d85
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-28 10:02:48 +0800

    added a DB migration strategy from v1 to v2

--- app/src/main/java/com/example/myapp/DB/local/appDatabase.java
+++ app/src/main/java/com/example/myapp/DB/local/appDatabase.java
@@ -6,12 +6,13 @@ import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Record.class, Tag.class}, version = 1, exportSchema = false)
+@Database(entities = {Record.class, Tag.class}, version = 2, exportSchema = false)
 public abstract class appDatabase extends RoomDatabase {
     public abstract TagDao tagDao();
     public abstract RecordDAO recordDAO();
@@ -26,6 +27,7 @@ public abstract class appDatabase extends RoomDatabase {
                 if (instance == null) {
                     instance = Room.databaseBuilder(c.getApplicationContext(),
                             appDatabase.class, "app_database")
+                            .addMigrations(MIGRATION_1_2)
                             .addCallback(initialCallback)
                             .build();
                 }
@@ -46,6 +48,28 @@ public abstract class appDatabase extends RoomDatabase {
                 tagDao.addTag(new Tag("标签A", "描述A"));
                 tagDao.addTag(new Tag("标签B", "描述B"));
             });
+        }
+    };
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            // "rename column" not available yer
+//            database.execSQL("ALTER TABLE tag RENAME COLUMN Tag TO `Tag name`;");
+
+            // update manually
+
+            // first, create a temp table with the new schema
+            database.execSQL("create table tag_new (`Tag name` TEXT NOT NULL, `Tag description` TEXT, primary key (`Tag name`));");
+
+            // copy data
+            database.execSQL("insert into tag_new (`Tag name`, `Tag description`) select Tag, `Tag description` from tag;");
+
+            // remove old table
+            database.execSQL("drop table tag");
+
+            // rename new table
+            database.execSQL("alter table tag_new rename to tag");
         }
     };
 }
