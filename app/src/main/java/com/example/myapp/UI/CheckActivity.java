65311fcdfaf11056f409e5c9813b1929869fce8f
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-28 16:16:50 +0800

    finished implementing CheckActivity

--- app/src/main/java/com/example/myapp/UI/CheckActivity.java
+++ app/src/main/java/com/example/myapp/UI/CheckActivity.java
@@ -1,16 +1,50 @@
 package com.example.myapp.UI;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapp.R;
 
-class CheckActivity extends AppCompatActivity {
+public class CheckActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check);
+
+        Button all = findViewById(R.id.check_record_all_button);
+        Button tag = findViewById(R.id.check_record_by_tag_button);
+        Button back = findViewById(R.id.check_record_back_button);
+
+        all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context c = v.getContext();
+                Intent i = new Intent(c, CheckDisplayActivity.class);
+                startActivity(i);
+            }
+        });
+
+        tag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context c = v.getContext();
+                Intent i = new Intent(c, CheckByTagChooseActivity.class);
+                startActivity(i);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
