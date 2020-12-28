8e9364da7fc2760d8e8afeb5afa19b0f0fde91f1
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-28 10:42:52 +0800

    added a method to add new record to DB, a method to delete record(s) in DB, and wrapped all return values in livedata

--- app/src/main/java/com/example/myapp/DB/local/RecordDAO.java
+++ app/src/main/java/com/example/myapp/DB/local/RecordDAO.java
@@ -1,43 +1,60 @@
 package com.example.myapp.DB.local;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.ColumnInfo;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
 @Dao
 public interface RecordDAO {
     @Query("select * from record")
-    List<Record> getAllRecords();
+    LiveData<List<Record>> getAllRecords();
 
     @Query("select * from record where ID = :rid")
-    Record getRecordById(int rid);
+    LiveData<Record> getRecordById(int rid);
 
     @Query("select * from record where ID in (:rids)")
-    List<Record> getRecordsById(int[] rids);
+    LiveData<List<Record>> getRecordsById(int[] rids);
 
     @Query("select * from record where Province = :province")
-    List<Record> getRecordsByProvince(String province);
+    LiveData<List<Record>> getRecordsByProvince(String province);
 
     @Query("select * from record where Province in (:provinces)")
-    List<Record> getRecordsByProvinces(String[] provinces);
+    LiveData<List<Record>> getRecordsByProvinces(String[] provinces);
 
     @Query("select * from record where City = :city")
-    List<Record> getRecordsByCity(String city);
+    LiveData<List<Record>> getRecordsByCity(String city);
 
     @Query("select * from record where City in (:cities)")
-    List<Record> getRecordsByCities(String[] cities);
+    LiveData<List<Record>> getRecordsByCities(String[] cities);
 
     @Query("select * from record where District = :district")
-    List<Record> getRecordsByDistrict(String district);
+    LiveData<List<Record>> getRecordsByDistrict(String district);
 
     @Query("select * from record where District in (:districts)")
-    List<Record> getRecordsByDistricts(String[] districts);
+    LiveData<List<Record>> getRecordsByDistricts(String[] districts);
 
     @Query("select * from record where Tag = :tag")
-    List<Record> getRecordsByTag(String tag);
+    LiveData<List<Record>> getRecordsByTag(String tag);
 
     @Query("select * from record where Tag in (:tags)")
-    List<Record> getRecordsByTags(String[] tags);
+    LiveData<List<Record>> getRecordsByTags(String[] tags);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void addRecord(Record record);
+
+//    @Update
+//    void update
+
+    @Delete
+    void deleteRecord(Record record);
+
+    @Query("delete from record")
+    void deleteAll();
 }
