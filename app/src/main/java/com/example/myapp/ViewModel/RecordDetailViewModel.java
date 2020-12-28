ca5af613e4c9f48f4600a8c206c4e00eefed7df3
chen-dudu <liguoc@student.unimelb.edu.au>
2020-12-28 18:01:55 +0800

    added view model for RecordDetailActivity

---
+++ app/src/main/java/com/example/myapp/ViewModel/RecordDetailViewModel.java
@@ -0,0 +1,26 @@
+package com.example.myapp.ViewModel;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.myapp.DB.local.Record;
+import com.example.myapp.Repository.Manager;
+
+public class RecordDetailViewModel extends ViewModel {
+
+    private Manager manager;
+
+    public RecordDetailViewModel(Application application) {
+        this.manager = new Manager(application);
+    }
+
+    public LiveData<Record> getRecord(int id) {
+        return manager.getRecordById(id);
+    }
+
+    public void delete(Record record) {
+        manager.deleteRecord(record);
+    }
+}
