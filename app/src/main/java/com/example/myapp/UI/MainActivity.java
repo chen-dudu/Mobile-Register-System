6216f1906e84897811f30978daeb54157b8da40d
chen-dudu <liguoc@student.unimelb.edu.au>
2021-01-04 17:37:10 +0800

    added required permission for bmob DB during app startup

--- app/src/main/java/com/example/myapp/UI/MainActivity.java
+++ app/src/main/java/com/example/myapp/UI/MainActivity.java
@@ -1,42 +1,38 @@
 package com.example.myapp.UI;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.viewpager.widget.PagerAdapter;
 
 import android.Manifest;
-import android.content.ContentProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.myapp.R;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Arrays;
 
 import cn.bmob.v3.Bmob;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_EXPORT = 1;
+    public static final String[] PERMISSIONS_NEEDED = {
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.WAKE_LOCK,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions(PERMISSIONS_NEEDED, REQUEST_CODE_EXPORT);
+        // init Bmob DB
         Bmob.initialize(this, "82326b7d373f04180abfe46ece1c3de4");
     }
 
@@ -46,16 +42,19 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == REQUEST_CODE_EXPORT) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                proceedExport(this);
+                for (int grantResult : grantResults) {
+                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                        finish();
+                        break;
+                    }
+                }
             }
             else {
-                int duration = Toast.LENGTH_SHORT;
-                Toast t = Toast.makeText(this, "未获得权限，导出终止", duration);
-                t.show();
+                finish();
             }
         }
         else {
-            System.out.println(">>> wrong request code");
+            finish();
         }
     }
 
@@ -85,16 +84,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickExport(View view) {
         Context c = view.getContext();
-        if (ContextCompat.checkSelfPermission(c, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            proceedExport(c);
-        }
-        else {
-            requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_EXPORT);
-        }
-    }
-
-    private void proceedExport(Context context) {
-        Intent i = new Intent(context, ExportActivity.class);
-        startActivity(i);
+        Intent i = new Intent(c, ExportActivity.class);
+        c.startActivity(i);
     }
 }
