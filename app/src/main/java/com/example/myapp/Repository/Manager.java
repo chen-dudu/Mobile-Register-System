9cdf1b92a5fcd67e114c18e05b5648845811e87d
chen-dudu <liguoc@student.unimelb.edu.au>
2021-01-05 10:36:01 +0800

    added a method to retrieve all tags from cloud DB

--- app/src/main/java/com/example/myapp/Repository/Manager.java
+++ app/src/main/java/com/example/myapp/Repository/Manager.java
@@ -1,8 +1,10 @@
 package com.example.myapp.Repository;
 
 import android.app.Application;
+import android.util.MutableBoolean;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.example.myapp.DB.local.Record;
 import com.example.myapp.DB.local.RecordDAO;
@@ -12,9 +14,13 @@ import com.example.myapp.DB.local.TagDao;
 import com.example.myapp.DB.local.appDatabase;
 import com.example.myapp.Util.RequestCallBack;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.datatype.BmobQueryResult;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SQLQueryListener;
 import cn.bmob.v3.listener.SaveListener;
 
 public class Manager {
@@ -29,8 +35,28 @@ public class Manager {
         recordDAO = db.recordDAO();
     }
 
+    // local
+//    public LiveData<List<Tag>> getAllTags() {
+//        return tagDao.getAllTags();
+//    }
+
+    // cloud
     public LiveData<List<Tag>> getAllTags() {
-        return tagDao.getAllTags();
+        MutableLiveData<List<Tag>> result = new MutableLiveData<>();
+//        List<Tag> tags = new ArrayList<>();
+        result.postValue(null);
+        String query = "select * from TagCloud order by name";
+        new BmobQuery<TagCloud>().doSQLQuery(query, new SQLQueryListener<TagCloud>() {
+            @Override
+            public void done(BmobQueryResult<TagCloud> bmobQueryResult, BmobException e) {
+                List<Tag> temp = new ArrayList<>();
+                for (TagCloud t: bmobQueryResult.getResults()) {
+                    temp.add(new Tag(t.getName(), t.getDescription()));
+                }
+                result.postValue(temp);
+            }
+        });
+        return result;
     }
 
     public LiveData<Tag> getTag(String tagName) {
